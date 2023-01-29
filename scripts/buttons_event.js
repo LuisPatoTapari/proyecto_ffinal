5e4bab3f4bb7ab8c6d681760abb9aee37c55cf63
LuisPatoTapari <123894441+LuisPatoTapari@users.noreply.github.com>
2023-01-29 14:45:20 -0300

    Add files via upload

---
+++ scripts/buttons_event.js
@@ -0,0 +1,45 @@
+/* Datos personales:*/
+function actualizar_contenido_1(){
+    var myDiv = document.getElementById("content");
+    myDiv.innerHTML = "<p>Edad: 40 años, Casado, </P>"
+    +"Hijos 2";
+}
+
+
+/**
+ *     Más de 20 años de experiencia en la venta de Indumentaria
+    Femenina Minorista y Mayorista.
+    Cartera de clientes en el Interior.
+    Cartera de Proveedores nacionales y en el exterior.
+    Líder en la organización de plantilla de vendedores, fijación de metas
+    y alcances de objetivos.
+    Manejo de Stock mínimo, máximo, reposición, crítico, flujo dinámico de mercaderías.
+    Reposición de Stock.
+    Stock mínimo en finalización de temporada
+
+ */
+function actualizar_contenido_2(){
+        var myDiv = document.getElementById("content");
+        myDiv.innerHTML = " Más de 20 años de experiencia en la venta de Indumentaria"
+    +"Femenina Minorista y Mayorista."
+   + "Cartera de clientes en el Interior."
+  +" Cartera de Proveedores nacionales y en el exterior."
+    +"Líder en la organización de plantilla de vendedores, fijación de metas"
+    +"y alcances de objetivos."
+    +"Manejo de Stock mínimo, máximo, reposición, crítico, flujo dinámico de mercaderías."
+  +  " Reposición de Stock."
+    +"Stock mínimo en finalización de temporada";
+}
+
+function actualizar_contenido_3(){
+        var myDiv = document.getElementById("content");
+        myDiv.innerHTML = "Licenciado en Marketing "
+        +" Especializado en telemarkin"; 
+        
+}
+function actualizar_contenido_4(){
+    var myDiv = document.getElementById("content");
+    myDiv.innerHTML = "Gerente Comercial";
+    
+    
+}
